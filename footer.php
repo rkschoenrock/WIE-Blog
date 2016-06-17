f0b480579ec1f083abb6182c461551fb9f6711c0
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-06-17 19:09:55 -0400

    update font and style footer

--- footer.php
+++ footer.php
@@ -1,15 +1,19 @@
 <footer class="row">
     <div class="four columns">
         <?php dynamic_sidebar('footer-one'); ?>
-</div>
+    </div>
     <div class="four columns">
         <?php dynamic_sidebar('footer-two'); ?>
-</div>
+    </div>
     <div class="four columns">
         <?php dynamic_sidebar('footer-three'); ?>
-</div>
+    </div>
+    <div class="twelve columns">
+        <p class="footer-left">&copy; 2016 Rebecca Schoenrock</p>
+        <p class="footer-right">Smaller Nav Here</p>
+    </div>
 </footer>
-</footer>
+
 
 </div> <!-- End Container Div -->
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" type="text/css" href="
         <?php bloginfo('stylesheet_url'); ?>
     " />
+    <link href='https://fonts.googleapis.com/css?family=Work+Sans:300' rel='stylesheet' type='text/css'>
 </head>
 <body>
     <div class="container"> <!-- Begin Container Div -->
