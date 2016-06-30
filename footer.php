3064bdf920da0c2f92d42276c0e464024f5718df
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-06-29 21:28:41 -0400

    Style the footer

--- footer.php
+++ footer.php
@@ -1,16 +1,14 @@
 <footer class="row">
-    <div class="four columns">
-        <?php dynamic_sidebar('footer-one'); ?>
-    </div>
-    <div class="four columns">
-        <?php dynamic_sidebar('footer-two'); ?>
-    </div>
-    <div class="four columns">
-        <?php dynamic_sidebar('footer-three'); ?>
-    </div>
     <div class="twelve columns">
-        <p class="footer-left">&copy; 2016 Rebecca Schoenrock</p>
-        <p class="footer-right">Smaller Nav Here</p>
+        <p>&copy; 2016 Rebecca Schoenrock
+        <span class="footer-right">
+			<?php
+				wp_nav_menu(array(
+					'sort_column' => 'menu_order',
+					'container_class' => 'blank-menu-footer'
+				));
+			?>
+		</span></p>
     </div>
 </footer>
 
